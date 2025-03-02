c406157d45d14c49975052aa55e441cb95b1d765
Hanuman Limbalkar <149709960+Hanuman-Limbalkar@users.noreply.github.com>
2025-03-02 11:04:09 +0530

    Update MainPage.jsx

--- Frontend/src/MainPage.jsx
+++ Frontend/src/MainPage.jsx
@@ -22,7 +22,7 @@ function MainPage() {
   async function reviewCode() {
     setIsLoading(true); // Show loader
     try {
-      const response = await axios.post("http://localhost:3001/ai/get-review", { code });
+      const response = await axios.post("https://ai-powered-code-reviewer-fmua.onrender.com", { code });
       setReview(response.data);
     } catch (error) {
       setReview("❌ Error: Unable to fetch review. Please try again.");
